88dc9c3adaaf46d2ce6787fcc8505cfd389d655b
AhmedSamiir51 <88041263+AhmedSamiir51@users.noreply.github.com>
2022-03-11 19:03:01 +0200

    fiexed issus

--- src/app/page/about-us/about-us.component.ts
+++ src/app/page/about-us/about-us.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { tileLayer, marker, icon, latLng } from 'leaflet';
 
 @Component({
   selector: 'app-about-us',
@@ -10,43 +9,7 @@ export class AboutUsComponent implements OnInit {
 
   constructor() { }
 
-    // Define our base layers so we can reference them multiple times
-    streetMaps = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      detectRetina: true,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
-    wMaps = tileLayer('http://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
-      detectRetina: true,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    });
-  
-    // Marker for the parking lot at the base of Mt. Ranier trails
-    paradise = marker([ 31.232483838632387, -330.05391598097054], {
-      icon: icon({
-        iconSize: [ 25, 41 ],
-        iconAnchor: [ 13, 41 ],
-        iconUrl: 'leaflet/marker-icon.png',
-        iconRetinaUrl: 'leaflet/marker-icon-2x.png',
-        shadowUrl: 'leaflet/marker-shadow.png'
-      })
-    });
-  
-    // Layers control object with our two base layers and the three overlay layers
-    layersControl = {
-      baseLayers: {
-        'Street Maps': this.streetMaps,
-        'Wikimedia Maps': this.wMaps
-      },
-    };
-  
-  
-  
-    options = {
-      layers: [ this.streetMaps ],
-      zoom: 7,
-      center: latLng(31.232483838632387, -330.05391598097054),
-    };
-  
+
 
   ngOnInit(): void {
   }
